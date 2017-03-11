a9322fbc6707925b2b6c42f052a627b892a669c1
Jackie Makdah <jackiemakdah7@gmail.com>
2017-03-10 22:17:25 -0600

    updated county.py

--- app/crawlers/county.py
+++ app/crawlers/county.py
@@ -1,19 +1,28 @@
-#from app import app, mysql
+import sys
+#goes back two levels so that we can import app 
+sys.path.append('../../')
+
+from app import app, mysql
+from app.crawlers.mappings import map_months
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait 
 import re
 
+
+map_times = { 
+    "7:00pm" : "19:00:00"
+    }
+
 def crawl():
-  connection = mysql.get_db()
-  cursor = connection.cursor()
+  #connection = mysql.get_db()
+  #cursor = connection.cursor()
 
   urls = ["http://www.visitchampaigncounty.org/events/category/14/arts-and-theater", "http://www.visitchampaigncounty.org/events/category/22/exhibits", "http://www.visitchampaigncounty.org/events/category/21/family---friendly", "http://www.visitchampaigncounty.org/events/category/16/festivals-and-fairs", "http://www.visitchampaigncounty.org/events/category/71/food-and-drink"] 
   driver = webdriver.PhantomJS()
   
   for url in urls:
     driver.get(url)
-    #WebDriverWait(driver, 10)
     soup = BeautifulSoup(driver.page_source, 'html.parser')
     #events = soup.find_all("div", class_="col-sm-2")
     #print(len(events))
@@ -23,17 +32,64 @@ def crawl():
       title = list_.find("a").contents[0]
       print(title)
       event_urls = list_.find("a")["href"]  
-      print("http://www.visitchampaigncounty.org/" + event_urls)
       driver.get("http://www.visitchampaigncounty.org/" + event_urls)
       event_soup = BeautifulSoup(driver.page_source, "html.parser")
       #print(event_soup.find("h4"))
       location_general = event_soup.find("li", class_="box place border-right").getText() 
-      print (location_general)
+      building = location_general.split('\n')[2]
+      addrAndStreet = location_general.split('\n')[3] 
+      city = location_general.split('\n')[4].split(',')[0]
+      zipcode = location_general.split('\n')[4].split(" ")[2]
+
+      print(building)
+      print(addrAndStreet)
+      print(city)
+      print(zipcode + '\n')
 
       time_general = event_soup.find("li", class_="box date border-right").getText()
-      print(time_general)
+      print((time_general).split('\n'))
+      year = time_general.split('\n')[2].split(" ")[3]
+      month =time_general.split('\n')[2].split(" ")[1]
+      #eventually use map_months 
+      if month == 'March':
+        month = '03'
+      if month == 'April':
+        month = '04'
 
+      date = time_general.split('\n')[2].split(" ")[2][:-1]
+
+      full_date = "{}-{}-{}".format(year, month, date) 
+      print(full_date)
+  
+      start_time = "00:00:00"
+      end_time = "00:00:00"
+      if "Starts" in time_general.split('\n')[3]:
+        start_time = time_general.split('\n')[3].split(" ")[2]
+        print("start_time" +start_time)
+
+        if start_time == "7:00pm":
+          start_t = map_times[start_time]
+          print("start_t:" + start_t)
+        
+      else:
+        start_time = time_general.split('\n')[3].split(" ")[0]
+        print("start_time: " + start_time)
+        end_time = time_general.split('\n')[3].split(" ")[2]
+        print("end_time: " + end_time)
+
+    
+      low_price = 0;
+      high_price = 0;
       price_general =  event_soup.find("li", class_="box tickte border-right").getText()
       print (price_general)
+      price_split = (price_general.split('\n'))[2]
+      if 'Buy Tickets' in price_split:
+        ticket_url = event_soup.find("li", class_="box tickte border-right").find("span", class_="site-link").find("a")["href"]
+        print(ticket_url)
+         
+
+      description =  event_soup.find("div", class_="panel-body").getText()
+      print(description)
+
 if __name__ == "__main__":
   crawl()
