213b9f91a0055299707476c329f93379b3e38904
Jackie Makdah <jackiemakdah7@gmail.com>
2017-03-10 22:46:45 -0600

    modified mapping.py

--- app/crawlers/mappings.py
+++ app/crawlers/mappings.py
@@ -1,19 +1,55 @@
-map_months = { 	"january" : "01", "jan" : "01",
-			"february" : "02", "feb" : "02",
-			"march" : "03", "mar" : "03",
-			"april" : "04", "apr" : "04",
-			"may" : "05",
-			"june" : "06", "jun" : "06",
-			"july" : "07", "jul" : "07",
-			"august" : "08", "aug" : "08",
-			"september" : "09", "sep" : "09", "sept" : "09",
-			"october" : "10", "oct" : "10",
-			"november" : "11", "nov" : "11",
-			"december" : "12", "dec" : "12"
-}
+
+map_months= {  "january" : "01", "jan" : "01", "January" : "01",
+    "february" : "02", "feb" : "02", "February" : "02",
+    "march" : "03", "mar" : "03", "March" : "03",
+    "april" : "04", "apr" : "04", "April" : "04",
+    "may" : "05", "May" : "05",
+    "june" : "06", "jun" : "06", "June" : "06",
+    "july" : "07", "jul" : "07", "July" : "07", 
+    "august" : "08", "aug" : "08", "August" : "08", 
+    "september" : "09", "sep" : "09", "sept" : "09", "September" : "09",
+    "october" : "10", "oct" : "10", "October" : "10",
+    "november" : "11", "nov" : "11", "November" : "11",
+    "december" : "12", "dec" : "12", "December" : "12"
+    }
+
+map_times = {
+    
+
+    }
 
 map_categories = {
-}
+    "sports" : "Sports",
+    "health & wellness" : "Health & Wellness",
+    "outdoors & recreation" : "Outdoors",
+    "kids & family" : "Family",
+    "neighborhood" : "Family",
+    "fundraising & charity" : "Family",
+    "university & alumni" : "University",
+    "other & miscellaneous" : "Other",
+    "art galleries & exhibits" : "Arts & Theatre",
+    "performing arts" : "Arts & Theatre",
+    "science" : "Academic",
+    "education" : "Academic",
+    "literary & books" : "Academic",
+    "holiday" : "Holiday",
+    "concerts & tour dates" : "Music"
+    #"" : "Technology",
+    #"" : "Cultural",
+    #"" : "Government",
+    #"" : "Home/Lifestyle",
+
+
+    }
 
 map_event_types = {
-}
+    "concerts & tour dates" : "Concert",
+    "conferences & tradeshows" : "Conference",
+    "business & networking" : "Networking & Career Fairs",
+    "art galleries & exhibits" : "Galleries & Exhibits",
+    "fundraising & charity" : "Charity"
+    #"" : "Talk",
+    #"" : "Festivals & Fairs"
+    }
+
+
