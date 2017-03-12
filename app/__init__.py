cb697024d50f9101aa2e79ffba098d22ec993897
Matt Swiszcz <mswis731@gmail.com>
2017-03-12 14:15:26 -0500

    more app init changes

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,34 @@
 from flask import Flask
 from flaskext.mysql import MySQL
-#import app.secret
 import os
 
-#SE_DIR = os.path.dirname(os.path.dirname(__file__))
-#PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-url = os.environ['DATABASE_URL']
-
+db_user = None
+db_password = None
+db_name = None
+db_host = None
+try:
+	import app.secret
+	db_user = secret.user
+	db_password = secret.password
+	db_name = secret.db
+	db_host = secret.host
+except ImportError:
+	url = os.environ['DATABASE_URL']
+	url_1 = url.split('/')
+	url_2 = url_1[2].split('@')
+	url_3 = url_2[0].split(':')
+	
+	db_user = url_3[0]
+	db_password = url_3[1]
+	db_name = url_1[3].split('?')[0]
+	db_host = url_2[1]
+	
 app = Flask(__name__)
 
-app.config['MYSQL_DATABASE_USER'] = secret.user
-app.config['MYSQL_DATABASE_PASSWORD'] = secret.password
-app.config['MYSQL_DATABASE_DB'] = secret.db
-app.config['MYSQL_DATABASE_HOST'] = secret.host
+app.config['MYSQL_DATABASE_USER'] = db_user
+app.config['MYSQL_DATABASE_PASSWORD'] = db_password
+app.config['MYSQL_DATABASE_DB'] = db_name
+app.config['MYSQL_DATABASE_HOST'] = db_host
 mysql = MySQL()
 mysql.init_app(app)
 
