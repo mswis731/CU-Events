f9f2988d7836013ac48ca9e9b1b25b84caf6399a
Jackie Makdah <jackiemakdah7@gmail.com>
2017-03-13 20:14:27 -0500

    wrote query to search for events

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from flask import render_template
-from app import app
+from app import app, mysql
 from app.crawlers.eventful import crawl as eventful_crawl
+import sys
 
 @app.route('/')
 @app.route('/index')
@@ -19,6 +20,49 @@ def sign_up():
 def browse():
     return render_template('browse.html')
 
+@app.route('/music/')
+def music():
+	connection = mysql.get_db()
+	cursor = connection.cursor()
+	cursor.execute("SELECT name FROM Category")
+	result = cursor.fetchall()
+	categories = [row[0] for row in result]
+	cursor.close()
+	return render_template('browse.html', categories=categories, result=result)
+
+@app.route('/browse')
+def get_types():
+	connection = mysql.get_db()
+	cursor = connection.cursor()
+	cursor.execute("SELECT name FROM EventType")
+	result = cursor.fetchall()
+	types = [row[0] for row in result]
+	cursor.close()
+	return render_template('browse.html', types=types, result=result)
+
+@app.route('/<category>')
+def find_cat(category):
+	connection = mysql.get_db()
+	cursor = connection.cursor()
+	cursor.execute("SELECT * FROM Event WHERE (name, startDate, startTime) IN (SELECT eventName, eventStartDate, eventStartTime FROM HasCategory WHERE categoryName='{}')".format(category))
+	events = [dict(name=row[0],
+                   description=row[1],
+                   building=row[2],
+                   addrAndStreet=row[3],
+                   city=row[4],
+                   zipcode=row[5],
+                   startDate=row[6],
+                   startTime=row[7],
+                   endDate=row[8],
+                   endTime=row[9],
+                   lowPrice=row[10],
+                   highPrice=row[11],
+                   nonUserViews=row[12]) for row in cursor.fetchall()]
+
+	return render_template('temp.html', events=events)
+
 @app.route('/crawl')
 def crawl():
 	return eventful_crawl()
+
+
